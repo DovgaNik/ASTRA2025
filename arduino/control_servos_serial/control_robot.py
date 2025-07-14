90b2533ede9f31295e76ce89e407937aa862e3c9
DovgaNik <nikita.dovgan.05@hotmail.com>
2025-07-14 15:57:34 +0300

    Added functions to open and close the clamp.

--- arduino/control_servos_serial/control_robot.py
+++ arduino/control_servos_serial/control_robot.py
@@ -6,19 +6,24 @@ import serial
 #B
 #C
 #D - clamp servo in the current setup
+CLAMP_SIGNAL='D'
 
 # Clamp is closed at ≈50º, we assume that the open position is ≈100º
+
+
+def clamp_open():
+    send_text(CLAMP_SIGNAL, 100)
+
+def clamp_close():
+    send_text(CLAMP_SIGNAL, 42)
+
+def send_text(servo_code, angle):
+    ser.write((servo_code + str(angle) + '\n').encode())
 
 ser = serial.Serial('/dev/ttyACM0', 9600)  # open serial port
 time.sleep(3)
 print(ser.name)
-ser.write('D100\n'.encode())
-time.sleep(2)
-ser.write('D50\n'.encode())
+
 ser.close()
 
-def clamp_open():
-    send_text()
 
-def send_text(command):
-    ser.write((command + '\n').encode())
