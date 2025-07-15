3bb9f5be39532122c4f76d7ec88c9db2fbec5280
DovgaNik <nikita.dovgan.05@hotmail.com>
2025-07-15 12:25:30 +0300

    Some additonal work on serial-servo control.

--- arduino/control_servos_serial/control_robot.py
+++ arduino/control_servos_serial/control_robot.py
@@ -1,27 +1,29 @@
 import time
 import serial
 
+ser = serial.Serial('/dev/ttyACM0', 9600)  # open serial port
+time.sleep(3)
+
+def send_text(servo_code, angle):
+    ser.write((servo_code + str(angle) + '\n').encode())
+
+
 # Pins of servos must be set up in the arduino code, while in python code the letter corresponding to each servo must be specified.
-#A
+#A - is the servo that control rotation of the whole arm (movement left-right)
+LR_SIGNAL='A'
 #B
 #C
 #D - clamp servo in the current setup
 CLAMP_SIGNAL='D'
-
-# Clamp is closed at ≈50º, we assume that the open position is ≈100º
-
-
 def clamp_open():
     send_text(CLAMP_SIGNAL, 100)
 
 def clamp_close():
     send_text(CLAMP_SIGNAL, 42)
 
-def send_text(servo_code, angle):
-    ser.write((servo_code + str(angle) + '\n').encode())
+# Clamp is closed at ≈50º, we assume that the open position is ≈100º
 
-ser = serial.Serial('/dev/ttyACM0', 9600)  # open serial port
-time.sleep(3)
+
 print(ser.name)
 
 ser.close()
